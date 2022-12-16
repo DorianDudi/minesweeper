185e12864ebeb92af942e39c3d0fc99df910ca6c
Dorian-DV <ddorianvlad@yahoo.ro>
2022-12-16 20:55:28 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,19 +6,19 @@ let game_matrix = [
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
 let mine_coordinates = [
 	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ];
 
 function getRandomIntInclusive(min, max) {
@@ -29,12 +29,12 @@ function getRandomIntInclusive(min, max) {
 
 function reveal_cell(elem_id) {
 	document.getElementById(elem_id).className = "cell_clicked";
-    let x = parseInt(elem_id[0]), y = parseInt(elem_id[1]), cell_value = game_matrix[x][y];
-    if (cell_value > 0) {
-        update_HTML_ID(x, y, cell_value);
-    } else {
-    	
-    }
+	let x = parseInt(elem_id[0]), y = parseInt(elem_id[1]), cell_value = game_matrix[x][y];
+	if (cell_value > 0) {
+		update_HTML_ID(x, y, cell_value);
+	} else {
+		// call fill function
+	}
 }
 
 function generate_mine_coordinates() {
@@ -52,38 +52,35 @@ function generate_mine_coordinates() {
 
 function update_HTML_ID(a, b, val) {
 	let divID = "" + a + b;
-	console.log(divID);
-    if (val != 9) {
-    	document.getElementById(divID).innerHTML = "<p>" + val + "</p>";
-    } else {
-    	document.getElementById(divID).innerHTML = '<img src="https://iili.io/HoDm1Og.png" border="0" width="38" height="38" />';
-    }
+	//console.log(divID);
+	if (val != 9) {
+		document.getElementById(divID).innerHTML = "<p>" + val + "</p>";
+	} else {
+		document.getElementById(divID).innerHTML = '<img src="https://iili.io/HoDm1Og.png" border="0" width="38" height="38" />';
+	}
 }
 
 function generate_game() {
-    for (let i = 0; i < 10; ++i) {
-    	let x = mine_coordinates[0][i], y = mine_coordinates[1][i];
-        game_matrix[x][y] = 9;
-    }
-    for (let i = 1; i <= 9; ++i) {
-        for (let j = 1; j <= 9; ++j) {
+	for (let i = 0; i < 10; ++i) {
+		let x = mine_coordinates[0][i], y = mine_coordinates[1][i];
+		game_matrix[x][y] = 9;
+	}
+	for (let i = 1; i <= 9; ++i) {
+		for (let j = 1; j <= 9; ++j) {
 			if (game_matrix[i][j] != 9) {
-            	let cell_value = 0;
-                for (let x = i - 1; x <= i + 1; ++x) {
-                	for (let y = j - 1; y <= j + 1; ++y) {
-                		if (game_matrix[x][y] == 9) {
-                        	++cell_value;
-                        }
-                	}
-                }
-                game_matrix[i][j] = cell_value;
-            }
-        }
-    }
+				let cell_value = 0;
+				for (let x = i - 1; x <= i + 1; ++x) {
+					for (let y = j - 1; y <= j + 1; ++y) {
+						if (game_matrix[x][y] == 9) {
+						++cell_value;
+					}
+				}
+			}
+			game_matrix[i][j] = cell_value;
+			}
+		}
+	}
 }
-
-function display_empty_
-
 
 generate_mine_coordinates();
 generate_game();
