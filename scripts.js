0aa0234ae7f2841f4ca952300df8988c5a3af4f3
Dorian-DV <ddorianvlad@yahoo.ro>
2022-12-27 22:39:13 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -22,64 +22,10 @@ function initialize_game_matrix() {
 	}
 }	// the above code declares and initializes the game matrix and the coordinate matrix
 
-function toggle_flag(divID) {	// toggles flag on right clik - disables flagged cell
-	if (document.getElementById(divID).classList.contains('cell_unclicked')) {
-		if(document.getElementById(divID).innerHTML != '') { // if has flag
-			document.getElementById(divID).innerHTML = '';
-			if (document.getElementById("game_over").hasAttribute("hidden")) {
-				document.getElementById(divID).setAttribute("onclick", "reveal_cell(this.id)");
-			}
-		} else {
-			document.getElementById(divID).innerHTML = '<img src="https://iili.io/Hz9kofR.png" border="0" width="30" height="30" />';
-			document.getElementById(divID).removeAttribute("onclick");
-		}
-	}
-}
-
 function getRandomIntInclusive(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1) + min); // random number in interval - min-max inclusive
-}
-
-function update_HTML_ID(a, b, val) { //changes display style of clicked cell and displays values or mine picture
-	let divID = "" + a + b;
-	if (val != 9) {
-		document.getElementById(divID).innerHTML = "<p>" + val + "</p>";
-	} else {
-		document.getElementById(divID).innerHTML = '<img src="https://iili.io/HoDm1Og.png" border="0" width="38" height="38" />';
-		game_over(0);
-	}
-}
-
-function reveal_cell(elem_id) {		// reveals contents of clicked cells by calling update_HTML_ID() triggers the flood-fill if value is 0
-	document.getElementById(elem_id).classList.replace('cell_unclicked', 'cell_clicked');
-	let x = parseInt(elem_id[0]), y = parseInt(elem_id[1]), cell_value = game_matrix[x][y];
-	if (cell_value > 0) {
-		update_HTML_ID(x, y, cell_value);
-	} else {
-		fill_cleared_cells(x, y);
-		reveal_corners();
-	}
-	if (user_wins()) {
-		game_over(1);
-	}
-}
-
-function user_wins() {	// checks if all empty cells have been cleared
-	let cleared_cells = 0;
-	for (let i = 1; i <= 9; ++i) {
-		for (let j = 1; j <= 9; ++j) {
-			let elemID = "" + i + j;
-			if (document.getElementById(elemID).classList.contains('cell_clicked')) {
-				++cleared_cells;
-			}
-		}
-	}
-	if (cleared_cells == 71) {
-		return 1;
-	}
-	return 0;
 }
 
 function generate_mine_coordinates() {	// populates mine coordinate matrix with random non-identical values
@@ -115,6 +61,61 @@ function generate_game() {	//populates game matrix with values 1 to 9 (9 being t
 			}
 		}
 	}
+}
+
+function toggle_flag(divID) {	// toggles flag on right clik - disables flagged cell
+	if (document.getElementById(divID).classList.contains('cell_unclicked')) {
+		if(document.getElementById(divID).innerHTML != '') { // if has flag
+			document.getElementById(divID).innerHTML = '';
+			if (document.getElementById("game_over").hasAttribute("hidden")) {
+				document.getElementById(divID).setAttribute("onclick", "reveal_cell(this.id)");
+			}
+		} else {
+			document.getElementById(divID).innerHTML = '<img src="https://iili.io/Hz9kofR.png" border="0" width="30" height="30" />';
+			document.getElementById(divID).removeAttribute("onclick");
+		}
+	}
+}
+
+
+function reveal_cell(elem_id) {		// reveals contents of clicked cells by calling update_HTML_ID() triggers the flood-fill if value is 0
+	document.getElementById(elem_id).classList.replace('cell_unclicked', 'cell_clicked');
+	let x = parseInt(elem_id[0]), y = parseInt(elem_id[1]), cell_value = game_matrix[x][y];
+	if (cell_value > 0) {
+		update_HTML_ID(x, y, cell_value);
+	} else {
+		fill_cleared_cells(x, y);
+		reveal_corners();
+	}
+	if (user_wins()) {
+		game_over(1);
+	}
+}
+
+function update_HTML_ID(a, b, val) { //changes display style of clicked cell and displays values or mine picture
+	let divID = "" + a + b;
+	if (val != 9) {
+		document.getElementById(divID).innerHTML = "<p>" + val + "</p>";
+	} else {
+		document.getElementById(divID).innerHTML = '<img src="https://iili.io/HoDm1Og.png" border="0" width="38" height="38" />';
+		game_over(0);
+	}
+}
+
+function user_wins() {	// checks if all empty cells have been cleared
+	let cleared_cells = 0;
+	for (let i = 1; i <= 9; ++i) {
+		for (let j = 1; j <= 9; ++j) {
+			let elemID = "" + i + j;
+			if (document.getElementById(elemID).classList.contains('cell_clicked')) {
+				++cleared_cells;
+			}
+		}
+	}
+	if (cleared_cells == 71) {
+		return 1;
+	}
+	return 0;
 }
 
 function fill_cleared_cells(x, y) {
@@ -183,6 +184,19 @@ function fill_cleared_cells(x, y) {
 	}
 }
 
+function reveal_corners() { // auxiliary function used by fill_cleared_cells() that handles the corners of the filled area
+	for (let i = 1; i < 10; ++i) {
+		for (let j = 1; j < 10; ++j) {
+			if (game_matrix[i][j] == 0) {
+				let currentID = "" + i + j;
+				if (document.getElementById(currentID).classList.contains('cell_clicked')) {
+					display_adjacents_of(i, j);
+				}
+			}
+		}
+	}
+}
+
 function display_adjacents_of(x, y) {  // displays the 3x3 submatrix that has the cell of x,y coordinates at it's center
 	for (let i = x - 1; i <= x + 1; ++i) {
 		for (let j = y - 1; j <= y + 1; ++j) {
@@ -191,19 +205,8 @@ function display_adjacents_of(x, y) {  // displays the 3x3 submatrix that has th
 				document.getElementById(currentID).classList.replace('cell_unclicked', 'cell_clicked');
 				if (game_matrix[i][j] > 0) {
 					document.getElementById(currentID).innerHTML = "<p>" + game_matrix[i][j] + "</p>";
-				}
-			}
-		}
-	}
-}
-
-function reveal_corners() { // auxiliary function used by fill_cleared_cells() that handles the corners of the filled area
-	for (let i = 1; i < 10; ++i) {
-		for (let j = 1; j < 10; ++j) {
-			if (game_matrix[i][j] == 0) {
-				let currentID = "" + i + j;
-				if (document.getElementById(currentID).classList.contains('cell_clicked')) {
-					display_adjacents_of(i, j);
+				} else {
+					fill_cleared_cells(i, j)
 				}
 			}
 		}
@@ -232,7 +235,7 @@ function game_over(user_won) { // changes icon at game over
 		for (let i = 1; i <= 9; ++i) {
 			for (let j = 1; j <= 9; ++j) {
 				let elemID = "" + i + j;
-				if (game_matrix[i][j] == 9) {
+				if (game_matrix[i][j] == 9) { // show all mines on user loss
 					document.getElementById(elemID).classList.replace('cell_unclicked', 'cell_clicked');
 					document.getElementById(elemID).innerHTML = '<img src="https://iili.io/HoDm1Og.png" border="0" width="38" height="38" />';
 				}
